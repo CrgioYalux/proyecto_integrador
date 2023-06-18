22718546b54e33990478cef15063b97707677e80
Sergio Yalux <randaledomman1@gmail.com>
2023-06-18 19:43:33 -0300

    fix: paths in home page

--- client/src/pages/Admin/Home/index.tsx
+++ client/src/pages/Admin/Home/index.tsx
@@ -4,8 +4,7 @@ import logoImgUrl from '../../../assets/TrendStyle.png';
 import Button from '../../../components/Button';
 import { Link } from 'react-router-dom';
 
-interface HomeProps {
-};
+interface HomeProps {};
 
 const Home: React.FC<HomeProps> = ({ }) => {
     const [state, actions] = useAdminSystem();
@@ -18,25 +17,20 @@ const Home: React.FC<HomeProps> = ({ }) => {
             </div>
             <div className='Home_container'>
                 <Button className='Home_linkButton'>
-                    <Link to="/admin/ventas">VENTAS</Link>
-                </Button>
-
-                <Button className='Home_linkButton'>
-                    <Link to="/admin/asientos">ASIENTOS</Link>
+                    <Link to="/admin/sales">SALES</Link>
                 </Button>
                 <Button className='Home_linkButton'>
-                    <Link to="/admin/compras">COMPRAS</Link>
+                    <Link to="/admin/accounting_entries">ACCOUNTING ENTRIES</Link>
                 </Button>
                 <Button className='Home_linkButton'>
-                    <Link to="/admin/registro">REGISTRO COMPRA</Link>
+                    <Link to="/admin/purchases">PURCHASES</Link>
                 </Button>
                 <Button className='Home_linkButton'>
-                    <Link to="/admin/inventario">INVENTARIO</Link>
+                    <Link to="/admin/inventory">INVENTORY</Link>
                 </Button>
                 <Button className='Home_linkButton'>
-                    <Link to="/admin/cuentas">CUENTAS</Link>
+                    <Link to="/admin/accounts">ACCOUNTS</Link>
                 </Button>
-                
             </div>
             <Button className='Home_logOutButton' onClick={() => actions.userSession.logOut()}>log out</Button>
         </div>

54dc5d06d9be7f7f1c9e8fed6a11f31580406221
Sergio Yalux <randaledomman1@gmail.com>
2023-06-19 03:47:08 -0300

    feat: create a SidePanel component for app settings

---
+++ client/src/components/SidePanel/index.tsx
@@ -0,0 +1,50 @@
+import './SidePanel.css';
+
+import { useState } from 'react';
+import { useAdminSystem } from '../../providers/AdminSystem';
+
+import Button from "../Button";
+import SwitchThemeBT from "../SwitchThemeBT";
+import HamburgerMenu from '../Icons/HamburgerMenu';
+
+import logoImgUrl from '../../assets/TrendStyle.png';
+
+interface SidePanelProps {
+    className?: string;
+};
+
+const SidePanel: React.FC<SidePanelProps> = ({  }) => {
+    const [sidePanelVisibility, setSidePanelVisibility] = useState<boolean>(false);
+    const [state, actions] = useAdminSystem();
+
+    const sessionInfoText: string = state.userSession.logged
+        ? `Logged as ${state.userSession.loginInfo?.username}`
+        : 'Not logged';
+
+    return (
+        <label className='SidePanel_label' htmlFor='SidePanel'>
+            <input 
+            id='SidePanel'
+            type='checkbox'
+            checked={sidePanelVisibility}
+            onChange={() => setSidePanelVisibility(prev => !prev)}
+            />
+            <HamburgerMenu className='SidePanel_label_icon' />
+            <div className='SidePanel'>
+                <div className='SidePanel_session_info'>
+                    <div className='SidePanel_logo'>
+                        <img src={logoImgUrl} />
+                    </div>
+                    <span>{sessionInfoText}</span>
+                </div>
+                <div className='SidePanel_options'>
+                    <SwitchThemeBT className='SidePanel_options_bt' />
+                    <Button className='SidePanel_options_bt SidePanel_options_lang_bt'>ES</Button>
+                    {state.userSession.logged && <Button className='SidePanel_options_bt SidePanel_options_logout_bt' onClick={() => actions.userSession.logOut()}>Log out</Button>}
+                </div>
+            </div>
+        </label>
+    );
+};
+
+export default SidePanel;

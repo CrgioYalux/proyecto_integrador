958ae56830b30697afab201d2713c3d77de9dc28
Sergio Yalux <randaledomman1@gmail.com>
2023-06-27 03:38:42 -0300

    feat: add base routes and express config

---
+++ server/src/config/express.ts
@@ -0,0 +1,37 @@
+import cors from 'cors';
+import express from 'express';
+import { router } from '../api/router';
+
+import { NOT_FOUND_PAGE } from './const';
+
+import type { Express } from 'express';
+
+const createServer = (): Express => {
+    const app = express();
+
+    app.disable('x-powered-by');
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(cors());
+
+    router.all('/api/**', (req, _, next) => {
+        console.log(`
+            ${req.method} ${req.url} at ${Date.now()}
+        `);
+        next();
+    });
+
+    app.use(router);
+
+    app.get('/ping', (_, res) => {
+        res.status(200).send('pong').end();
+    });
+
+    app.get('/**', (_, res) => {
+        res.status(404).send(NOT_FOUND_PAGE).end();
+    });
+
+    return app;
+};
+
+export { createServer };
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,13 @@
 import http from 'http';
-import cors from 'cors';
-import express from 'express';
+import { createServer } from './config/express';
 
-const app = express();
-app.use(cors({ origin: '*' }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT ?? '4000';
+const HOST = process.env.host ?? 'localhost';
 
-app.get('/ping', (req, res) => {
-    res.status(200).send('pong').end();
-});
-
-const PORT = process.env.PORT ?? 4000;
-const server = http.createServer(app);
-
-server.listen(PORT, () => {
-    console.log(`Server listening on port:${PORT}`);
-});
-
+(() => {
+    const app = createServer();
+    const server = http.createServer(app);
+    server.listen(PORT, () => {
+        console.log(`Server listening on http://localhost:${PORT}/`);
+    });
+})();

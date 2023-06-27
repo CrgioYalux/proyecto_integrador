d43c5f91de6cca7b331a1ad20c723d9b81053ed6
Nacho <naxtim14@gmail.com>
2023-06-27 12:09:16 -0300

    feat: Update the Modal components

--- client/src/pages/Admin/Sales/SalesNew/index.tsx
+++ client/src/pages/Admin/Sales/SalesNew/index.tsx
@@ -1,6 +1,4 @@
 import './SalesNew.css'
-import Providers from '../../Purchases/PurchasesNew/components/Providers';
-import Input from '../../../../components/InputsSales';
 import {useState} from 'react';
 import BtnModal from '../../../../components/Modal';
 
@@ -28,65 +26,37 @@ const SalesNew: React.FC<SalesNewProps> = ({ }) => {
             
 
             <form className="FormContainer" onSubmit={event=> event.preventDefault()}>
-
-                <div className='ProviderContainer'>
                     
-                </div>
-
-                <div className="containerDer">
-                    <input type="text" className="Inputs" placeholder="Nombre y Apellido" />
-                    <input type="date" className="Inputs" placeholder="Fechaaa" /> 
-                    <input type="text" className="Inputs" placeholder="Localidad" />
-                    <input type="text" className="Inputs" placeholder="Descripcion" />
-                    
-                    <div className="containerCheckBox">
-                      <label className="containerLabel">
-                            <input type="checkbox"/>
-                            <span className="checkmark"></span>
-                            One
-                      </label>
-                    </div>
-                </div>
-                <div className="ContainerIzq">
-                    
-                    <input type="text" className="Inputs" placeholder="Metodo de Pago" />
-
-                        <div className="containerVender">
-
-                            <input type="text" className="Inputs" placeholder="Cantidad" />
-                            <input type="text" className="Inputs" placeholder="Importe" />
-                            <input type="text" className="Inputs" placeholder="Total" />
-                            
+                        <div className="containerVender">    
                             <BtnModal nameBtn = "Aceptar" btnModalName = "Venta">
 
-                            <div className='containerBill'>
-                                <h1>Tipo de Factura</h1>
-                                <button onClick={selectBillA} className='ButtonBill'>
-                                    <span>A</span>
-                                </button>
-                                <button onClick={selectBillB} className='ButtonBill'>
-                                    <span>B</span>
-                                </button>
-                                <span>Factura {typeBill}</span>
-                            </div>
-                                <div className="containerInputModal">
+                                    <div className='containerBil'>
+                                        <button onClick={selectBillA} className='ButtonBill'>
+                                            <span>A</span>
+                                        </button>
+                                        <button onClick={selectBillB} className='ButtonBill'>
+                                            <span>B</span>
+                                        </button>
+                                        <span>Factura {typeBill}</span>
+                                    </div>
+                                    <div className="containerInputModal">
+                                            <h1>Cliente</h1>
+                                            <div className="line"></div>
+                                            <label htmlFor="text" className='TextLabel'>Apellido y Nombre</label>
+                                            <input type="text" className='Inputs' />
+                                            <label htmlFor="text" className='TextLabel'>CUIT</label>
+                                            <input type="text" className='Inputs' />
+                                            <label htmlFor="text" className='TextLabel'>Direccion</label>
+                                            <input type="text" className='Inputs' />
+                                            <label htmlFor="date" className='TextLabel'>Fecha</label>
+                                            <input type="date" className='Inputs' />
+                                            <div className="line"></div>
+                                            <h1>Detalle</h1>
+                                                
+                                    </div>
 
-                                    <input type="text" className="InputsModal" placeholder="Nombre y Apellido" />
-                                    <input type="date" className="InputsModal" placeholder="Fechaaa" /> 
-                                    <input type="text" className="InputsModal" placeholder="Localidad" />
-                                    <input type="text" className="InputsModal" placeholder="Descripcion" />
-                                    {typeBill === "A" && <input type="text" className="InputsModal" placeholder="Cuit" />}
-                                    {typeBill === "A" && <input type="text" className="InputsModal" placeholder="Precio Unitario" />}
-                                </div>
                             </BtnModal>
-
-                        </div>
-                    
-
-                    
-                </div>
-
-                
+                        </div> 
             </form>
 
 
